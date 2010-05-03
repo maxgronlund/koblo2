013a34582b9b50b5ad0cd16cbe30a9a71ff7a4be
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-05-03 21:17:51 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta3'
+gem 'mysql'
+gem 'devise', '1.1.rc1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '0910f3b4bcb0363634c11abfa44a2f0205e856e243b8e716f15f33820424c0f95724b1cdd2f37c0f507cf47f09a009564b23ffae51cd747b85d925ff790e4e3f'
