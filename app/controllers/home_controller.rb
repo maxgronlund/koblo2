88b6c99a63fab8a7e7da7099f451c389bc92a0f6
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-05-31 21:37:50 +0200

    More song scopes supported

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
 
   def index
-    @songs = Song.best.paginate :page => (params[:page] || 1), :per_page => 5
+    scope = %w{best newest most_downloaded}.include?(params[:scope]) ? params[:scope] : 'best'
+    @songs = Song.send(scope).paginate :page => (params[:page] || 1), :per_page => 5
   end
 
 end
