c33bf0846a0c13816a9c648e2a45b2624a297233
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-06-01 13:47:13 +0200

    Basic join

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+    // Popups
+    $('#join_link').click(function () {
+        $('#join_popup_div').show();
+    });
+    $('#popup_close').click(function () {
+        $('#join_popup_div').hide();
+    });
+
+    $(".check_box").click(function () {
+        if ($(this).attr('src') == '/images/icons/check_box_arrow.gif') {
+            $(this).attr('src', '/images/icons/check_box.gif');
+        } else {
+            $(this).attr('src', '/images/icons/check_box_arrow.gif');
+        }
+    });
+
+});
+
