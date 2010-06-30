c506909107afd3fc9d8246097ec6ed00946c6f73
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-06-30 10:52:43 +0200

    More follow/unfollow stuff

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -6,13 +6,13 @@ class ConnectionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_id(params[:user_id])
+    user = User.find_by_id(params[:id])
     current_user.follow(user)
     redirect_to user_path(user)
   end
 
   def destroy
-    user = User.find_by_id(params[:user_id])
+    user = User.find_by_id(params[:id])
     current_user.stop_following(user)
     redirect_to user_path(user)
   end
