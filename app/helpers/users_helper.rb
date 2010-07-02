af543366e9d0052c44df58ba96d0991b56ac8080
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-07-02 10:01:24 +0200

    Now highlighting in the user section works

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
   def user_tab(name, url)
     css_class = 'cufon'
+    css_class += ' selected' if current_page?(url)
     content_tag(:li, :class => css_class) {
       link_to(name, url)
     }
