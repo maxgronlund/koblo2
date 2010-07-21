fb46dce5ea256e881db3d200e5f96805609e434f
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-07-21 12:08:53 +0200

    Fixed spaces in file names issues

--- lib/audio_converter.rb
+++ lib/audio_converter.rb
@@ -1,3 +1,4 @@
+require 'open3'
 class AudioConverter
   @queue = :audio_conversion
 
@@ -7,8 +8,8 @@ class AudioConverter
     unless track.mp3?
       mp3_file = '/tmp/' + filename_with_no_extension + ".mp3"
       `rm -f #{mp3_file}`
-      stdout = `lame #{track.uploaded_data.path} #{mp3_file} 2>&1`
-      unless stdout =~ /Warning: unsupported audio format/
+      stdin, stdout, stderr = Open3.popen3('lame', track.uploaded_data.path, mp3_file)
+      unless stderr.readlines(' ') =~ /Warning: unsupported audio format/
         track.mp3 = File.new(mp3_file)
         track.save
         track.mp3.instance_write(:content_type, 'audio/mpeg')
@@ -18,8 +19,8 @@ class AudioConverter
     unless track.wav?
       wav_file = '/tmp/' + filename_with_no_extension + ".wav"
       `rm -f #{wav_file}`
-      stdout = `ffmpeg -i #{track.uploaded_data.path} #{wav_file} 2>&1`
-      unless stdout =~ /Audio encoding failed/
+      stdin, stdout, stderr = Open3.popen3('ffmpeg', '-i', track.uploaded_data.path, wav_file)
+      unless stderr.gets =~ /Audio encoding failed/
         track.wav = File.new(wav_file)
         track.save
       end
