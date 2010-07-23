329864c7228f8a201fe5c78153343fccc9588c22
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-07-23 11:39:04 +0200

    Logged in user is now redirected to latest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    redirect_to songs_path(:scope => 'best')
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    else
+      redirect_to songs_path(:scope => 'best')
+    end
   end
 
 end
