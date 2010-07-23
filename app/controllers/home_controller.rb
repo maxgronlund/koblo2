55741245aaba476b0697a25db2a176047df9609c
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-07-23 13:59:57 +0200

    Fixed home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    if user_signed_in?
+    if user_signed_in? && !params[:redirect]
       redirect_to user_path(current_user)
     else
       redirect_to songs_path(:scope => 'best')
