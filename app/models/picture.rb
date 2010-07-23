efcf0ca0706c2cbb6e7127bfe79e351dd8045237
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-07-23 11:41:40 +0200

    BW thumbs

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,8 @@
 class Picture < ActiveRecord::Base
 
-  has_attached_file :picture, :styles => { :profile => '200x200#', :profile_bw => "200x200#", :thumb => '48x48#' },
+  has_attached_file :picture, :styles => { :profile => '200x200#', :profile_bw => "200x200#", :thumb => '48x48#', :thumb_bw => '48x48#' },
                               :default_url => "/images/dummy_:style.png",
-                              :convert_options => { :profile_bw => "-colorspace Gray" }
+                              :convert_options => { :profile_bw => "-colorspace Gray", :thumb_bw => "-colorspace Gray" }
 
   delegate :url, :to => :picture
 
