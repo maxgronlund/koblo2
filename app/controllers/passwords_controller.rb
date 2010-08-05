ad898acb77fc50a86567cedc7f2e4a273919690a
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-08-05 16:13:11 +0200

    Password reset page now works and is styled

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,16 @@
 class PasswordsController < Devise::PasswordsController
   before_filter :sidebar_for_frontpage
   layout 'frontpage_content'
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+
+    if resource.errors.empty?
+      set_flash_message :notice, :send_instructions
+      redirect_to root_path
+    else
+      render_with_scope :new
+    end
+  end
+  
 end
