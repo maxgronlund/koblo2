1bc13b1f6c7ecc2b488882c40224c6c23bd26c39
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-08-09 12:27:38 +0200

    Updated with stereo waveforms

--- lib/create_waveform.rb
+++ lib/create_waveform.rb
@@ -3,6 +3,13 @@ require 'narray'
 require 'RMagick'
 include Magick
 
+class Array
+  def mean 
+    sum / size.to_f
+  end
+end
+
+
 class CreateWaveform
   @queue = :audio_conversion
 
@@ -12,8 +19,63 @@ class CreateWaveform
   def self.perform(track_id)
     track = Track.find(track_id)
     file = track.uploaded_data.path
-    buckets = fill_buckets WIDTH,file
-    gc = build_graph buckets,WIDTH,HEIGHT
+
+    # Read the raw data
+    bytes = sox_get_bytes(file)
+
+    # Use only absolute values
+    bytes.map!(&:abs)
+
+    # Split in left and right channel
+    b = false
+    left_channel, right_channel = bytes.partition { b = !b }
+
+    # Create the graph
+    gc = Magick::Draw.new  
+    gc.stroke('#01bec2')
+    gc.stroke_width(1)
+    midpoint = HEIGHT/2
+    size = left_channel.size
+    sample_point = (size / (WIDTH * 4.0)).to_i
+    puts sample_point
+
+    prev_x = 0.0
+    prev_y = midpoint
+    j = 0
+    max = 0
+    left_channel.each_with_index do |sample, i|
+      if i % sample_point == 0
+        max = sample > max ? sample : max
+        j += 1
+        if j % 4 == 0
+          x = WIDTH.to_f*i/size
+          y = midpoint + HEIGHT*(max/32768.0)/2.0
+          gc.line(prev_x, prev_y, x, y)
+          max = 0
+          prev_x = x
+          prev_y = y
+        end
+      end
+    end
+
+    prev_x = 0.0
+    prev_y = midpoint
+    j = 0
+    max = 0
+    right_channel.each_with_index do |sample, i|
+      if i % sample_point == 0
+        max = sample > max ? sample : max
+        j += 1
+        if j % 4 == 0
+          x = WIDTH.to_f*i/size
+          y = midpoint - HEIGHT*(max/32768.0)/2.0
+          gc.line(prev_x, prev_y, x, y)
+          max = 0
+          prev_x = x
+          prev_y = y
+        end
+      end
+    end
 
     canvas = Magick::Image.new(WIDTH, HEIGHT) do
       self.background_color = 'transparent'
@@ -29,32 +91,17 @@ class CreateWaveform
     track.save
   end
 
-  # fill the buckets
-  def self.fill_buckets width,file
-    buckets = NArray.int(width,2)
-    #let sox fetch the byte array
-    bytes = sox_get_bytes file
-    bucket_size = (((bytes.size-1).to_f / width)+0.5).to_i + 1
-    (0..bytes.total-1).each do |i|
-        value = bytes[i]
-        index = i/bucket_size
-        #minimum
-        buckets[index,0] = value if value < buckets[index,0]
-        #maximum
-        buckets[index,1] = value if value > buckets[index,1]
-    end
-    return buckets
-  end
-
   # open file with sox and return a byte array with sweet waveform information in it
   def self.sox_get_bytes file
     x=nil
     # read a 16 bit linear raw PCM file
     if file.ends_with?('mp3')
-      sox_command = [ 'sox', '-t', 'mp3', file, '-t', 'raw', '-r', '500', '-c', '1', '-s', '-L', '-' ]
+      sox_command = [ 'sox', '-t', 'mp3', file]
     else
-      sox_command = [ 'sox', file, '-t', 'raw', '-r', '500', '-c', '1', '-s', '-L', '-' ]
+      sox_command = [ 'sox', file] 
     end
+    sox_command += ['-t', 'raw', '-r', '500', '-s', '-L', '-' ]
+
     # we have to fork/exec to get a clean commandline
     IO.popen('-') { |p|
       if p.nil? then
@@ -68,24 +115,7 @@ class CreateWaveform
       puts "sox returned no data, command was\n> #{sox_command.join(' ')}"
       exit 1
     end
-    return NArray.to_na(x.unpack("s*"))
-  end
-
-  # build the waveform graph
-  def self.build_graph buckets,size,height
-    gc = Magick::Draw.new  
-    offset = 0.0
-    scale = 32768/height
-    midpoint = height/2
-
-    (0..(buckets.size/2)-1).each do |i|
-      low = buckets[i,0]
-      high = buckets[i,1]
-      gc.stroke('#01bec2')
-      gc.stroke_width(1)
-      gc.line(i+offset, midpoint+low/scale, i+offset, midpoint+high/scale)
-    end
-    return gc
+    return x.unpack("s*")
   end
   
 end
