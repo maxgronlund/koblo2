37c6e92087441976f8aaa7a42304bb4144047e3b
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-09-16 13:04:08 +0200

    The pictures controller issue has been solved in the newest Rails version

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,9 +2,6 @@ class PicturesController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:destroy]
 
-  # Watch this https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/3913
-  skip_before_filter :verify_authenticity_token, :only => :create
-  
   def create
     @picture = Picture.new(params[:picture] || { :picture => params[:Filedata] })
     @picture.picture_content_type = MIME::Types.type_for(@picture.picture_file_name).to_s
