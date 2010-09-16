1abe192d0073ca916d49974aaca3cc1935a255e6
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-09-16 13:04:39 +0200

    The tracks controller issue has been solved in the newest Rails version

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,9 +2,6 @@ class TracksController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:create, :destroy]
 
-  # Watch this https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/3913
-  skip_before_filter :verify_authenticity_token, :only => :create
-  
   def create
     @track = current_user.tracks.build(params[:track])
     @track.uploaded_data_content_type = MIME::Types.type_for(@track.uploaded_data_file_name).to_s
