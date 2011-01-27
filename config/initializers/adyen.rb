bc822089219449bc042bf5f187eca47ba08e193f
Jørgen Orehøj Erichsen <joe@erichsen.net>
2011-01-27 08:34:07 +0100

    Fixed initializers

--- config/initializers/adyen.rb
+++ config/initializers/adyen.rb
@@ -1,2 +1,3 @@
-configuration = YAML.load(File.read(Rails.root + 'config/adyen.yml'))
-Adyen.load_config(configuration[Rails.env])
+Adyen.configuration.default_api_params = { :merchant_account => 'KrebsCoDK' }
+Adyen.configuration.api_username = 'ws@Company.KrebsCo'
+Adyen.configuration.api_password = 'moov3uCiqu'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '0910f3b4bcb0363634c11abfa44a2f0205e856e243b8e716f15f33820424c0f95724b1cdd2f37c0f507cf47f09a009564b23ffae51cd747b85d925ff790e4e3f'
+Rails.application.config.secret_token = '0910f3b4bcb0363634c11abfa44a2f0205e856e243b8e716f15f33820424c0f95724b1cdd2f37c0f507cf47f09a009564b23ffae51cd747b85d925ff790e4e3f2'
